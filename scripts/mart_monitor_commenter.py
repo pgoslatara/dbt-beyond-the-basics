99e2fa7bfa8fb392e70082404bd17048ddd9948f
pgoslatara <pgoslatara@gmail.com>
2023-05-29 14:29:02 +0000

    Handle large numbers

--- scripts/mart_monitor_commenter.py
+++ scripts/mart_monitor_commenter.py
@@ -222,7 +222,7 @@ def format_results(results: list) -> list:
                     ELSE '🔴'
                 END
                 || ' '
-                || CAST({metric} AS INTEGER)
+                || ROUND(CAST({metric} AS NUMERIC), 0)
                 || ' ('
                 || ROUND(diff_{metric}_pct, 2)
                 || '%)'
