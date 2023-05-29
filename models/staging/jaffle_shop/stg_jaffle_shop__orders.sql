774ff5546df5a820f19041b097c0a61df8bec00e
pgoslatara <pgoslatara@gmail.com>
2023-05-29 13:45:27 +0000

    Nicer mart monitor formatting

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -13,7 +13,7 @@ with
 
         select id as order_id, user_id as customer_id, order_date, status
         from source
-        where status <> "completed"
+        where status <> "returned"
 
     )
 
