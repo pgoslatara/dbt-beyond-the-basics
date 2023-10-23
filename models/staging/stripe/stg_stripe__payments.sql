bed4c2a6e26a061d9e131808c152d68029587da7
pgoslatara <pgoslatara@gmail.com>
2023-10-23 20:36:44 +0200

    Ensuring columns starting with 'is_' are boolean

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -15,6 +15,9 @@ with
             id as payment_id,
             order_id,
             payment_method,
+            case
+                when payment_method in ("coupon", "gift_card") then true else false
+            end as is_voucher,
 
             -- `amount` is currently stored in cents, so we convert it to dollars
             {{ cents_to_dollars('amount') }} as amount
